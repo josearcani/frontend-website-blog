4afbda786c527a6bcb81512be8665a5236de8541
josearc <juanjose.arcani2309@gmail.com>
2021-11-12 16:46:57 -0400

    Add google site verification meta tag to the headMetada.js component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
     "DEV_API_URL": "http://localhost:5000",
-    "PRODUCTION_API_URL": "https://www.api.josearc.me"
+    "PRODUCTION_API_URL": "https://www.api.josearc.me",
+    "GOOGLE_VERIFICATION_ID": "KCiTf04TIUsIwwepeEWy3rnEicBf6iJGi1MaMPU2Rdc"
   }
 }
